c8e2978fde8b46f357396bef50f8317300a53cbf
shibo <2990452909@qq.com>
2025-02-07 22:45:04 +0800

    windows compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "CApp.h"
 
+#ifdef _WIN32
+#undef main
+#elif defined(_WIN64)
+#undef main
+#endif // _WIN32
+
 int main(int argc, char* argv[]) {
-   CApp app;
-   return app.OnExecute();
+	CApp app;
+	return app.OnExecute();
 }
-
-
